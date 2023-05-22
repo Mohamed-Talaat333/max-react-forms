cc35ce940a6641a34e2e8f0edee7c4537806fc34
Mohamed Talaat <mohamedtalaat333@gmail.com>
2023-05-22 15:57:25 +0300

    Implementing name input state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,20 @@
-const SimpleInput = (props) => {
+import { useState } from "react";
+
+const SimpleInput = () => {
+  const [nameInputValue, setNameInputValue] = useState("");
+
+  const formSubmitHandler = async (event) => {
+    event.preventDefault();
+    const nameInputValueSet = event.target.name.value;
+    await setNameInputValue(nameInputValueSet);
+    console.log(nameInputValue);
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' name="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
